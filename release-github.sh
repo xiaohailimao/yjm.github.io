7f8bcefe414c5cf1539afcaa8a8c8f02dd04bb92
jiamei <328054241@qq.com>
2021-06-29 14:55:25 +0800

    deploy

--- release-github.sh
+++ release-github.sh
@@ -2,12 +2,12 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-git add -A
-git commit -m 'deploy'
-git push origin master
+# git add -A
+# git commit -m 'deploy'
+# git push origin master
 
-# 进入生成的文件夹
-cd ./blog
+# # 进入生成的文件夹
+# cd ./blog
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -15,6 +15,6 @@ cd ./blog
 git init
 git add -A
 git commit -m 'deploy'
-git push -u git@github.com:Jameel01/yjm.github.io.git master
+git push -u git@github.com:Jameel01/yjm.github.io.git master --fouce
 
 cd -
